f2ee7d27e54bfdb45485c9a8600a93ebcb1e48c4
zykure <>
2025-07-03 11:33:16 +0200

    Exit from build in case of errors

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Building PDF ..."
-pdflatex thebook.tex
-pdflatex thebook.tex
-makeindex thebook.idx
-pdflatex thebook.tex
+pdflatex thebook.tex || exit $?
+pdflatex thebook.tex || exit $?
+makeindex thebook.idx || exit $?
+pdflatex thebook.tex || exit $?
 
 echo "Building EPUB ..."
-tex4ebook -m index -c thebook.cfg -e thebook.mk4 -f epub thebook.tex
+tex4ebook -m index -c thebook.cfg -e thebook.mk4 -f epub thebook.tex || exit $?
 
 echo "Building MOBI ..."
-ebook-convert thebook.epub thebook.mobi
+ebook-convert thebook.epub thebook.mobi || exit $?
