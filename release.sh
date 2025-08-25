f537ea9e747099ed27bd68805d25e3484e4de3b5
zykure <>
2025-08-25 14:27:06 +0200

    Fix some indexed words

--- release.sh
+++ release.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 REL_NAME=$(date +'%Y.%m.%d')
+REL_TITLE="${REL_NAME}"
 REL_FILES="thebook.pdf thebook.epub thebook.mobi"
 
-REL_TITLE="${REL_NAME}"
-REL_NOTES=""
-
-gh release create ${REL_NAME} -t ${REL_TITLE} -n ${REL_NOTES} ${REL_FILES}
+gh release create "${REL_NAME}" -t "${REL_TITLE}" ${REL_FILES}
